1392d2a69ae4e303ff6f14263a1803c22b609254
Kevan Hollbach <khollbach@gmail.com>
2023-12-01 08:20:10 -0500

    1-2

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::{io, error::Error};
+use std::{io, error::Error, cmp::Reverse};
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn _part_1() -> Result<(), Box<dyn Error>> {
     let mut sum = 0;
     for line in io::stdin().lines() {
         let line = line?;
@@ -14,4 +14,72 @@ fn main() -> Result<(), Box<dyn Error>> {
     }
     dbg!(sum);
     Ok(())
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut sum = 0;
+    for line in io::stdin().lines() {
+        let line = line?;
+        let a = first_digit(&line);
+        let b = last_digit(&line);
+        let ab: String = [a, b].into_iter().collect();
+        let n: u32 = ab.parse().unwrap();
+        sum += n;
+    }
+    dbg!(sum);
+    Ok(())
+}
+
+const DIGITS: [(&str, char); 9] = [
+    ("one", '1'),
+    ("two", '2'),
+    ("three", '3'),
+    ("four", '4'),
+    ("five", '5'),
+    ("six", '6'),
+    ("seven", '7'),
+    ("eight", '8'),
+    ("nine", '9'),
+];
+
+fn first_digit(line: &str) -> char {
+    let mut candidates = vec![];
+
+    // Word digits.
+    for (s, d) in DIGITS {
+        if let Some(idx) = line.find(s) {
+            candidates.push((idx, d));
+        }
+    }
+
+    // Single-char digits.
+    let idx = line.find(|c: char| c.is_ascii_digit()).unwrap();
+    let digit = line.chars().nth(idx).unwrap();
+    candidates.push((idx, digit));
+
+    // Take the smallest idx.
+    candidates.sort();
+    let (_, d) = candidates[0];
+    d
+}
+
+fn last_digit(line: &str) -> char {
+    let mut candidates = vec![];
+
+    // Word digits.
+    for (s, d) in DIGITS {
+        if let Some(idx) = line.rfind(s) {
+            candidates.push((idx, d));
+        }
+    }
+
+    // Single-char digits.
+    let idx = line.rfind(|c: char| c.is_ascii_digit()).unwrap();
+    let digit = line.chars().nth(idx).unwrap();
+    candidates.push((idx, digit));
+
+    // Take the largest idx.
+    candidates.sort_by_key(|&pair| Reverse(pair));
+    let (_, d) = candidates[0];
+    d
 }
