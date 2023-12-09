bcc4041d30a316495118ef360cfeb0ac50bb9abe
Kevan Hollbach <khollbach@gmail.com>
2023-12-09 08:25:17 -0500

    8-2 (finally!)
    I was confusing winning_steps[0] with the cycle_length.
    
    The equations end up being way less scary after fixing that.

--- src/bin/day_8.rs
+++ src/bin/day_8.rs
@@ -1,4 +1,4 @@
-use std::{collections::{HashMap, HashSet}, io};
+use std::{collections::HashMap, io};
 
 use anyhow::{bail, ensure, Context, Result};
 use once_cell::sync::Lazy;
@@ -112,61 +112,25 @@ fn traverse_part_2(directions: &[Direction], graph: &Graph) -> usize {
     unreachable!();
 }
 
+/// Prints out some equations that can be solved by hand.
+///
+/// In particular, we want the LCM (least-common-multiple) of a few different
+/// cycle lengths. You can plug this into wolfram alpha.
 fn part_2(directions: &[Direction], graph: &Graph) -> Result<()> {
-    let mut sets = vec![];
     for start in graph.keys().filter(|s| s.ends_with('A')) {
         let r = find_repetition(directions, graph, start);
-        ensure!(r.winning_steps.len() == 1, "expecting one end node per start node");
-        let s = Set {
-            offset: r.repeated_step,
-            multiple: r.winning_steps[0].checked_sub(r.repeated_step).context("expected the end node to be repeated infinitely often")?,
-        };
-        sets.push(s);
+        dbg!(r);
     }
-
-    // Maybe brute force will work ?
-    let ans = intersection(sets.into_iter().map(Set::first_1m));
-    dbg!(ans);
-
     Ok(())
 }
 
-/// The set of all n such that `n = offset + multiple * x`, for some x >= 0.
-#[derive(Debug, Clone, Copy)]
-struct Set {
-    offset: usize,
-    multiple: usize,
-}
-
-impl Set {
-    fn first_1m(self) -> HashSet<usize> {
-        let mut out = HashSet::new();
-        let mut x = self.offset;
-        for _ in 0..1_000_000 {
-            out.insert(x);
-            x += self.multiple;
-        }
-        out
-    }
-}
-
-fn intersection(mut sets: impl Iterator<Item=HashSet<usize>>) -> HashSet<usize> {
-    let Some(mut out) = sets.by_ref().next() else {
-        return HashSet::new();
-    };
-    for s in sets {
-        out = out.intersection(&s).copied().collect();
-    }
-    out
-}
-
 #[derive(Debug)]
-// // TODO
-// // The compiler thinks these fields are never read, but it doesn't realize we're
-// // using the Debug impl to print them.
-// #[allow(dead_code)]
+// The compiler thinks these fields are never read, but it doesn't realize we're
+// using the Debug impl to print them.
+#[allow(dead_code)]
 struct Repetition {
-    repeated_step: usize,
+    stem_length: usize,
+    cycle_length: usize,
     winning_steps: Vec<usize>,
 }
 
@@ -186,7 +150,8 @@ fn find_repetition(directions: &[Direction], graph: &Graph, start: &str) -> Repe
         let state = State { node, dir_idx };
         if let Some(&repeated_step) = seen_states.get(&state) {
             return Repetition {
-                repeated_step,
+                stem_length: repeated_step,
+                cycle_length: step - repeated_step,
                 winning_steps,
             };
         }
