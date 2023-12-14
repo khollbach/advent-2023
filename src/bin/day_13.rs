9c5e51dc712087c44a36f2652bf7ed2c66604b82
Kevan Hollbach <khollbach@gmail.com>
2023-12-14 10:29:37 -0500

    13-2

--- src/bin/day_13.rs
+++ src/bin/day_13.rs
@@ -1,22 +1,27 @@
 use std::io;
 
-use anyhow::{bail, ensure, Result};
+use anyhow::{bail, ensure, Context, Result};
 use itertools::Itertools;
+
+#[allow(dead_code)]
+fn part_1() -> Result<()> {
+    let mut ans = 0;
+    for mut input in read_input()? {
+        match input.find_reflection()? {
+            Reflection::Row(r) => ans += r * 100,
+            Reflection::Col(c) => ans += c,
+        }
+    }
+    dbg!(ans);
+    Ok(())
+}
 
 fn main() -> Result<()> {
     let mut ans = 0;
     for mut input in read_input()? {
-        let row = input.find_row_reflection();
-        let col = input.find_col_reflection();
-        ensure!(
-            row.is_some() ^ col.is_some(),
-            "expected exactly one line of reflection"
-        );
-        if let Some(row) = row {
-            ans += 100 * row;
-        }
-        if let Some(col) = col {
-            ans += col;
+        match input.find_new_reflection()? {
+            Reflection::Row(r) => ans += r * 100,
+            Reflection::Col(c) => ans += c,
         }
     }
     dbg!(ans);
@@ -47,17 +52,56 @@ struct Input {
     grid: Vec<Vec<bool>>,
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Reflection {
+    Row(usize),
+    Col(usize),
+}
+
 impl Input {
-    fn find_row_reflection(&mut self) -> Option<usize> {
+    fn find_new_reflection(&mut self) -> Result<Reflection> {
+        let existing = self.find_reflection().context("unmodified")?;
+
+        let (nrows, ncols) = self.dims();
+        for i in 0..nrows {
+            for j in 0..ncols {
+                self.grid[i][j] ^= true;
+                for r in self.find_reflections() {
+                    if r != existing {
+                        return Ok(r);
+                    }
+                }
+                self.grid[i][j] ^= true; // restore orig state
+            }
+        }
+
+        bail!("failed to find smudge")
+    }
+
+    fn find_reflections(&mut self) -> Vec<Reflection> {
+        let rows = self.find_row_reflections();
+        let cols = self.find_col_reflections();
+
+        let rows = rows.into_iter().map(Reflection::Row);
+        let cols = cols.into_iter().map(Reflection::Col);
+        rows.chain(cols).collect()
+    }
+
+    fn find_reflection(&mut self) -> Result<Reflection> {
+        let out = self.find_reflections();
+        ensure!(out.len() == 1, "expected exactly one line of reflection");
+        Ok(out[0])
+    }
+
+    fn find_row_reflections(&mut self) -> Vec<usize> {
         let n = self.grid.len();
-        let mut out = None;
+        let mut out = vec![];
 
         for second_half in [false, true] {
             for i in 1..=n / 2 {
                 if self.is_mirrored_at(i) {
                     let row_idx = if second_half { n - i } else { i };
-                    debug_assert!(out.is_none(), "expected at most one line of reflection");
-                    out = Some(row_idx);
+                    out.push(row_idx);
                 }
             }
             self.grid.reverse(); // check the bottom half
@@ -76,16 +120,15 @@ impl Input {
         out
     }
 
-    fn find_col_reflection(&mut self) -> Option<usize> {
+    fn find_col_reflections(&mut self) -> Vec<usize> {
         self.transpose();
-        let out = self.find_row_reflection();
+        let out = self.find_row_reflections();
         self.transpose();
         out
     }
 
     fn transpose(&mut self) {
-        let nrows = self.grid.len();
-        let ncols = self.grid[0].len();
+        let (nrows, ncols) = self.dims();
 
         let mut out = vec![vec![false; nrows]; ncols]; // note the swap !
         for i in 0..nrows {
@@ -94,5 +137,11 @@ impl Input {
             }
         }
         self.grid = out;
+    }
+
+    fn dims(&self) -> (usize, usize) {
+        let nrows = self.grid.len();
+        let ncols = self.grid[0].len();
+        (nrows, ncols)
     }
 }
